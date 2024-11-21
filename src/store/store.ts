0d1200376569fbf8a288b83ff49882c084a09d42
omid <o.shali01@gmail.com>
2024-11-21 14:09:01 +0330

    fixed a bug in redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { cartSlice } from "./cart-slice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartSlice.reducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
