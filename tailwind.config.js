b68727cc2654f46b7b6756b62b48c5bf7aeeb5ce
omid <o.shali01@gmail.com>
2024-11-21 14:08:03 +0330

    related to base style commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+        "secondary-color": "var(--secondary-color)",
+        "backGround-color": "var(--backGround-color)",
+      },
+    },
   },
   plugins: [],
 };
