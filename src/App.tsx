caf0d26cccc36dacfdff661531166bd99cec2d41
omid <o.shali01@gmail.com>
2024-11-23 12:28:10 +0330

    Added modal cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Navbar } from "./components/navbar";
+import { ProductList } from "./components/product-list";
+
 function App() {
-  return <div className="bg-gray-100">Shopping cart</div>;
+  return (
+    <div className="flex flex-col items-center bg-backGround-color min-h-[100vh] text-white">
+      <div className="max-w-[80%] w-full">
+        <Navbar />
+        <div className="mt-[70px]">
+          <ProductList />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
